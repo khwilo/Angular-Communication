73ff2d5c2395c1effaf47494f579fa4cf0046852
Khwilo Kabaka <khwilowatai@gmail.com>
2019-11-23 12:45:08 +0300

    Communicating through a state management service

--- APM-Start/src/app/products/product-shell/product-shell-detail.component.ts
+++ APM-Start/src/app/products/product-shell/product-shell-detail.component.ts
@@ -1,17 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { ProductService } from '../product.service';
-import { IProduct } from '../product';
+import { Component, OnInit } from "@angular/core";
+import { ProductService } from "../product.service";
+import { IProduct } from "../product";
 
 @Component({
-    selector: 'pm-product-shell-detail',
-    templateUrl: './product-shell-detail.component.html'
+  selector: "pm-product-shell-detail",
+  templateUrl: "./product-shell-detail.component.html"
 })
 export class ProductShellDetailComponent implements OnInit {
-    pageTitle: string = 'Product Detail';
+  pageTitle: string = "Product Detail";
 
-    constructor(private productService: ProductService) { }
+  get product(): IProduct | null {
+    return this.productService.currentProduct;
+  }
 
-    ngOnInit() {
-    }
+  constructor(private productService: ProductService) {}
 
+  ngOnInit() {}
 }
--- APM-Start/src/app/products/product.module.ts
+++ APM-Start/src/app/products/product.module.ts
@@ -1,25 +1,28 @@
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
 
-import { SharedModule } from '../shared/shared.module';
+import { SharedModule } from "../shared/shared.module";
 
-import { ProductListComponent } from './product-list.component';
-import { ProductDetailComponent } from './product-detail.component';
-import { ProductEditComponent } from './edit/product-edit.component';
+import { ProductListComponent } from "./product-list.component";
+import { ProductDetailComponent } from "./product-detail.component";
+import { ProductEditComponent } from "./edit/product-edit.component";
 
-import { ProductService } from './product.service';
-import { ProductEditGuard } from './edit/product-edit-guard.service';
-import { ProductParameterService } from './product-parameter.service';
+import { ProductService } from "./product.service";
+import { ProductEditGuard } from "./edit/product-edit-guard.service";
+import { ProductParameterService } from "./product-parameter.service";
+import { ProductShellComponent } from "./product-shell/product-shell.component";
+import { ProductShellDetailComponent } from "./product-shell/product-shell-detail.component";
+import { ProductShellListComponent } from "./product-shell/product-shell-list.component";
 
 @NgModule({
   imports: [
     SharedModule,
     RouterModule.forChild([
-      { path: '', component: ProductListComponent },
-      { path: ':id', component: ProductDetailComponent },
+      { path: "", component: ProductShellComponent },
+      { path: ":id", component: ProductDetailComponent },
       {
-        path: ':id/edit',
-        canDeactivate: [ ProductEditGuard ],
+        path: ":id/edit",
+        canDeactivate: [ProductEditGuard],
         component: ProductEditComponent
       }
     ])
@@ -27,12 +30,11 @@ import { ProductParameterService } from './product-parameter.service';
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
-    ProductEditComponent
+    ProductEditComponent,
+    ProductShellComponent,
+    ProductShellDetailComponent,
+    ProductShellListComponent
   ],
-  providers: [
-    ProductService,
-    ProductEditGuard,
-    ProductParameterService
-  ]
+  providers: [ProductService, ProductEditGuard, ProductParameterService]
 })
-export class ProductModule { }
+export class ProductModule {}
